73daee9c9737e0084f89ff2b1b7a5e1f94debadb
Jengacat <bluehuman7708@gmail.com>
2024-06-01 16:43:24 +0900

    no message

---
+++ Rougelike_Project/Enemies/Goblin/goblinFSM.gd
@@ -0,0 +1,40 @@
+extends FiniteStateMachine
+
+func _init() -> void: #特定のクラスのインスタンスが生成される際に自動的に呼び出される
+	_add_state("idle")
+	_add_state("move")
+	_add_state("hurt")
+	_add_state("dead")
+
+func _ready() -> void: #ノードの初回更新時に呼ばれる
+	set_state(states.move)
+
+func _state_logic(_delta: float) -> void: #各状態ごとのロジック
+	if state == states.move:
+		parent.chase()
+		parent.move()
+
+
+func _get_transition() -> int: #状態遷移を決定するロジック
+	match state:
+		states.idle:
+			if parent.distance_to_player > parent.MAX_DISTANCE_TO_PLAYER or parent.distance_to_player < parent.MIN_DISTANCE_TO_PLAYER:
+				return states.move #状態がidleで、プレイヤーとの距離が近すぎたり遠すぎたりする場合は、状態を移動に変更する。
+		states.move:
+			if parent.distance_to_player < parent.MAX_DISTANCE_TO_PLAYER and parent.distance_to_player > parent.MIN_DISTANCE_TO_PLAYER:
+				return states.idle #状態がmoveで、プレイヤーとの距離が最大距離と最小距離の間にある場合、状態をアイドルに変更する。
+		states.hurt:
+			if not animation_player.is_playing():
+				return states.move #状態がfurtで、アニメーションが終わったら状態をmoveに戻す
+	return -1
+
+func _enter_state(_previous_state: int, new_state: int) -> void:
+	match new_state:
+		states.idle:
+			animation_player.play("idle")
+		states.move:
+			animation_player.play("walk")
+		states.hurt:
+			animation_player.play("hurt")
+		states.dead:
+			animation_player.play("dead")
---
+++ Rougelike_Project/Rooms.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+const SPAWN_ROOMS: Array = [preload("res://Rooms/SpawnRoom0.tscn"),preload("res://Rooms/SpawnRoom1.tscn")]
+const INTERMEDIATE_ROOMS: Array = [preload("res://Rooms/Room0.tscn"),preload("res://Rooms/Room1.tscn")]
+const END_ROOMS: Array = [preload("res://Rooms/EndRoom0.tscn")]
+
+const TILE_SIZE : int = 16
+
+@export var num_levels: int = 5
+
+#そのシーンにいたら取得をする
+@onready var player : CharacterBody2D = get_parent().get_node("Player")
+
+func _ready() -> void:
+	_spawn_rooms()
+	#ready関数で最初に部屋を生成する
+
+func _spawn_rooms() -> void:
+	var previous_room : Node2D
+	
+	for i in num_levels: #生成する部屋の数だけ繰り返す (default:5)
+		var room: Node2D
+		
+		if i == 0:
+			room = SPAWN_ROOMS[randi() % SPAWN_ROOMS.size()].instantiate()
+			player.position = room.get_node("PlayerSpawnPos").position
+		else:
+			if i == num_levels - 1:
+				room =END_ROOMS[randi() % END_ROOMS.size()].instantiate()
+			else: #1よりも上だと敵スポーン部屋が生成する
+				room = INTERMEDIATE_ROOMS[randi() % INTERMEDIATE_ROOMS.size()].instantiate()
+			
+			var previous_room_tilemap: TileMap = previous_room.get_node("TileMap")
+			var previous_room_door: StaticBody2D = previous_room.get_node("Doors/Door")
+			var exit_tile_pos: Vector2 = previous_room_tilemap.local_to_map(previous_room_door.position) + Vector2i.UP * 2
+			
+			#廊下生成をするためのタイル生成準備
+			var corridor_height : int = randi() % 5 + 2 #廊下の長さは6~1までの長さで生成される
+			for y in corridor_height: #for文でカウントされた数だけタイルを生成する(for文で反復処理)
+				previous_room_tilemap.set_cell(0, exit_tile_pos + Vector2(-2, -y), 0, Vector2(4,5))
+				previous_room_tilemap.set_cell(0, exit_tile_pos + Vector2(-1, -y), 0, Vector2(2,1))
+				previous_room_tilemap.set_cell(0, exit_tile_pos + Vector2(0, -y), 0, Vector2(2,1))
+				previous_room_tilemap.set_cell(0, exit_tile_pos + Vector2(1, -y), 0, Vector2(3,5))
+			
+			## 廊下生成に使用する変数(いじるな)## 
+			var room_tilemap : TileMap = room.get_node("TileMap")
+			var Multiplied_yTile = room_tilemap.get_used_rect().size.y * TILE_SIZE
+			var VertPlusTile = (1 + corridor_height) * TILE_SIZE
+			## ## ## ## ## ## ## ## ## ## ##
+			
+			#廊下生成
+			room.position = previous_room_door.global_position + Vector2.UP * Multiplied_yTile + Vector2.UP * VertPlusTile + Vector2.LEFT * room_tilemap.local_to_map(room.get_node("Entrance/Marker2D2").position).x * TILE_SIZE
+			
+		add_child(room)
+		previous_room = room
