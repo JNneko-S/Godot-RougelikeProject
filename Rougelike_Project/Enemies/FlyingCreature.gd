199886e919e4dc7be6a47c6efe7e3cd5302150fa
Jenga-S <126161166+JNneko-S@users.noreply.github.com>
2024-06-16 20:37:40 +0900

    Delete Rougelike_Project/Enemies/FlyingCreature.gd

--- Rougelike_Project/Enemies/FlyingCreature.gd
+++
@@ -1,6 +0,0 @@
-extends Enemy #親Character→Enemyから継承されている
-
-@onready var hitbox : Area2D = get_node("Hitbox")
-
-func _process(_delta : float) -> void:
-	hitbox.knockback_direction = velocity.normalized()
